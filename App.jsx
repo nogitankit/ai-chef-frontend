3f1dc5064757a05ed9434ea09a30931877b038cf
xyz <xyz@Aayushs-MacBook-Air.local>
2025-12-16 13:27:09 +0530

    added footer

--- App.jsx
+++ App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header'
 import Form from './components/Form'
+import Footer from './components/Footer'
 
 export default function App(){
     return(
         <>
             <Header />
             <Form />
+            <Footer />
         </>
     )
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return(
+        <footer className="footer" >
+            <small>© 2025 mayoJS development. No will to reserve rights.</small>
+        </footer>
+    )
+}
