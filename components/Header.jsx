59a1252730684033ec7c35a91a54320ef6e9750b
xyz <xyz@Aayushs-MacBook-Air.local>
2025-12-18 15:58:36 +0530

    dark mode prototype

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,18 @@
 import chefLogo from "/chef-logo.png"
 export default function Header() {
+
+    function toggleDarkmode(){
+        document.body.classList.toggle("dark-mode")
+    }
+
     return (
         <header className="header">
             <img src={chefLogo} alt="Chef Claude Logo" className="chef-logo" />
             <h1 className="title">AI Chef</h1>
+            <button className="light-dark"
+                    onClick={toggleDarkmode}
+            >light-mode
+            </button>
         </header>
     )
 }
